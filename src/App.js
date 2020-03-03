ba58efd8ff226ac9828a5b9983ef526c55e09caa
Chris Kalfas <christopherkalfas22@gmail.com>
2020-03-03 18:06:15 -0500

    Added Complete and Delete buttons and handlers to put a line through the todo or delete it from the list.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import './App.css'
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo, removeTodo }) {
   return(
-    <div className='todo'>
+    <div 
+      style={ { textDecoration: todo.isCompleted ? 'line-through' : " " } }
+      className='todo'>
       { todo.text }
+      <div>
+        <button onClick={ ()=> completeTodo(index) }>Complete</button>
+        <button onClick={ ()=> removeTodo(index) }>X</button>
+      </div>
     </div>
   )
 }
@@ -55,11 +61,30 @@ function App(){
     setTodos(newTodos)
   }
 
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+
   return(
     <div className='app'>
       <div className='todo-list'>
         { todos.map(( todo, index ) => (
-          <Todo key={ index } index={ index } todo={ todo } />
+          <Todo 
+            key={ index } 
+            index={ index } 
+            todo={ todo }
+            completeTodo={ completeTodo }
+            removeTodo={removeTodo}
+          />
         ))}
           <TodoForm addTodo={addTodo} />
       </div>
