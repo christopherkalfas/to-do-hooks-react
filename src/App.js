8b4f92a323d3513001f16b891d74ab8c131c5361
Chris Kalfas <christopherkalfas22@gmail.com>
2020-03-03 17:42:21 -0500

    Added input form. Wrote cool little conditional line that wont submit form if input field empty. Add took the form input and passed it to the NewTodo component in App parent. Also added a clear form on submit feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { useState } from 'react'
+import './App.css'
 
 function Todo({ todo, index }) {
   return(
     <div className='todo'>
       { todo.text }
     </div>
+  )
+}
+
+function TodoForm({ addTodo }) {
+  const [ value, setValue ] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(!value) return
+    addTodo(value)
+    setValue('')
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='input' 
+        value={value}
+        placeholder="Add New Todo!" 
+        onChange={ e => 
+          setValue(e.target.value)
+        }
+      />
+    </form>
   )
 }
 
@@ -24,12 +50,18 @@ function App(){
     },
   ])
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
   return(
     <div className='app'>
       <div className='todo-list'>
         { todos.map(( todo, index ) => (
           <Todo key={ index } index={ index } todo={ todo } />
         ))}
+          <TodoForm addTodo={addTodo} />
       </div>
     </div>
   )
